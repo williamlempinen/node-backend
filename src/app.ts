d32c9acf6e099a36a9e73415f6450395452bc6ea
William Lempinen <wlempin@gmail.com>
2024-08-24 14:01:58 +0300

    basics

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,11 @@
-import express, { nextfunction, request, response } from 'express'
+import express, { Request, Response, NextFunction } from 'express'
+import Logger from './core/Logger'
+import cors from 'cors'
 
 const app = express()
+
+Logger.info('Hello world')
+
+app.use(express.json())
+app.use(cors())
+app.use('/', routes)
