f7c868aa1b5afb61606a17f28f884d20501e8d27
William Lempinen <wlempin@gmail.com>
2024-08-29 14:47:12 +0300

    login success

--- src/routes/access/login.ts
+++ src/routes/access/login.ts
@@ -2,13 +2,32 @@ import express, { Request, Response } from 'express'
 import { validator } from '../../core/validator'
 import Access from './schema'
 import { asyncHandler } from '../../core/asyncHandler'
+import UserRepo from '../../database/repository/UserRepo'
+import Logger from '../../core/Logger'
 
 const router = express.Router()
 
 router.post(
   '/login',
   validator(Access.login),
-  asyncHandler(async (request: Request, response: Response) => {})
+  asyncHandler(async (request: Request, response: Response) => {
+    const results = await UserRepo.login(request.body)
+
+    if (!results) {
+      Logger.error('Error during login')
+      return response.status(500).json({ message: 'User login failed' })
+    }
+
+    const { user, accessToken, refreshToken } = results
+
+    Logger.info(`Login user: ${user}`)
+
+    return response.status(201).json({
+      user,
+      accessToken,
+      refreshToken
+    })
+  })
 )
 
 export default router
