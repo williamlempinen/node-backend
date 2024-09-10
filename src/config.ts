b3b33bc62679ec35fa78f2ddaa2e925014c147e4
William Lempinen <wlempin@gmail.com>
2024-09-10 17:49:44 +0300

    dev in container

--- src/config.ts
+++ src/config.ts
@@ -1,17 +1,14 @@
 import dotenv from 'dotenv'
-import path from 'path'
+dotenv.config()
 import Logger from './core/Logger'
 
-if (process.env.NODE_ENV === 'development') {
-  Logger.info('using local')
-  dotenv.config({ path: path.resolve(__dirname, '../.env.local') })
-} else {
-  dotenv.config({ path: path.resolve(__dirname, '../.env') })
-}
-
-export const port = process.env.PORT || 8000
+export const port = process.env.PORT!
 export const DATABASE_URL = process.env.DATABASE_URL!
 export const JWT_SECRET = process.env.JWT_SECRET!
 export const REDIS_HOST = process.env.REDIS_HOST!
 export const REDIS_PORT = process.env.REDIS_PORT!
 export const REDIS_URL = process.env.REDIS_URL!
+Logger.debug('Hello from config.ts')
+Logger.debug(
+  `port: ${port}, DATABASE_URL: ${DATABASE_URL}, JWT_SECRET: ${JWT_SECRET}, REDIS_PORT: ${REDIS_PORT}, REDIS_URL: ${REDIS_URL}`
+)
