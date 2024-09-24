82e63cd857472d570cb05b0a6814e99e2b24026e
William Lempinen <wlempin@gmail.com>
2024-09-24 21:07:05 +0300

    add type safety to asynchandler

--- src/core/asyncHandler.ts
+++ src/core/asyncHandler.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction } from 'express'
+import { Request, Response, NextFunction, RequestHandler } from 'express'
 
 type AsyncFunction = (request: Request, response: Response, next: NextFunction) => Promise<any>
 
-export const asyncHandler = (execution: AsyncFunction) => {
+export const asyncHandler = (execution: AsyncFunction): RequestHandler => {
   return (request: Request, response: Response, next: NextFunction) => {
-    execution(request, response, next).catch(next)
+    Promise.resolve(execution(request, response, next)).catch(next)
   }
 }
