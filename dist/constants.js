af2a998168a949fed7ccab0d82bae0a976aee4f3
William Lempinen <wlempin@gmail.com>
2024-12-14 17:00:37 +0200

    -

--- dist/constants.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-Object.defineProperty(exports, '__esModule', { value: true })
-exports.HOUR_NUM = exports.HOUR_STR = void 0
-exports.HOUR_STR = '3600'
-exports.HOUR_NUM = 3600
-//# sourceMappingURL=constants.js.map
